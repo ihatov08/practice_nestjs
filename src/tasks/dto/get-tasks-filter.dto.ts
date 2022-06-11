d8a4afb978d2a3176ccf4a4f5689f45e69fb0dbc
Tomoya Yoshida <ihatov08@gmail.com>
2022-06-12 01:11:13 +0900

    検索機能のvalidation実装

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,7 +1,13 @@
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { TaskStatus } from '../task.model';
 
 export class GetTasksFilterDto {
   // ?は省略可能なプロパティ
+  @IsOptional()
+  @IsEnum(TaskStatus)
   status?: TaskStatus;
+
+  @IsOptional()
+  @IsString()
   search?: string;
 }
