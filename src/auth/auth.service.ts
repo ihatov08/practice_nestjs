386679ebe0b3b9e181efefcae82c67edc023e8cb
Tomoya Yoshida <tomoya_yoshida@c.dwango.co.jp>
2022-07-07 15:35:36 +0900

    ユーザー登録時のvalidation実装

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
@@ -16,7 +16,16 @@ export class AuthService {
 
     const user = this.usersRepository.create({ username, password });
 
-    await this.usersRepository.save(user);
+    try {
+      await this.usersRepository.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+      console.log(error.code);
+    }
   }
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
