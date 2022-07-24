19cf3450d5636573c6d506107af747212cf9f2e4
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-24 20:22:33 +0900

    不要なファイル削除

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,50 +1,6 @@
 import { Module } from '@nestjs/common';
-import { TasksModule } from './tasks/tasks.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { DataSource } from 'typeorm';
-import { Task } from './tasks/tasks.entity';
-import { AuthModule } from './auth/auth.module';
-import { User } from './auth/user.entity';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { configValicationSchema } from './config.schema';
-import { SequelizeModule } from '@nestjs/sequelize';
-import { Config } from 'prettier';
-import { MenteesModule } from './mentees/mentees.module';
-import { Mentee } from './mentees/mentee.model';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      ignoreEnvFile: true,
-      validationSchema: configValicationSchema,
-    }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        type: 'postgres',
-        synchronize: true,
-        autoLoadEntities: true,
-        host: configService.get('POSTGRES_HOST'),
-        port: configService.get('POSTGRES_PORT'),
-        username: configService.get('POSTGRES_USERNAME'),
-        password: configService.get('POSTGRES_PASSWORD'),
-        database: configService.get('POSTGRES_DATABASE'),
-      }),
-    }),
-    SequelizeModule.forRoot({
-      dialect: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: Number(process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USERNAME,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DATABASE,
-      autoLoadModels: true,
-      synchronize: true,
-    }),
-    TasksModule,
-    AuthModule,
-    MenteesModule,
-  ],
+  imports: [],
 })
 export class AppModule {}
--- src/tasks/dto/update-task-status.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsEnum } from 'class-validator';
-import { TaskStatus } from '../task-status.enum';
-
-export class updateTaskStatusDto {
-  @IsEnum(TaskStatus)
-  status: TaskStatus;
-}
