13b581de211eb783c7972b27e2dc58388f941b71
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-24 11:40:28 +0900

    とりあえず起動できるところまで

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,32 +1,7 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { configValicationSchema } from './config.schema';
 import { EnvironmentConfigModule } from './infrastructure/config/environment-config/environment-config.module';
-import { TypeormModule } from './infrastructure/config/typeorm/typeorm.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      ignoreEnvFile: true,
-      validationSchema: configValicationSchema,
-    }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        type: 'postgres',
-        synchronize: true,
-        autoLoadEntities: true,
-        host: configService.get('POSTGRES_HOST'),
-        port: configService.get('POSTGRES_PORT'),
-        username: configService.get('POSTGRES_USERNAME'),
-        password: configService.get('POSTGRES_PASSWORD'),
-        database: configService.get('POSTGRES_DATABASE'),
-      }),
-    }),
-    EnvironmentConfigModule,
-    TypeormModule,
-  ],
+  imports: [EnvironmentConfigModule],
 })
 export class AppModule {}
