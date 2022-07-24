561c51ce9a61efde2111adae46a02732eb978ce6
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-24 10:26:36 +0900

    修正

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,15 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Logger, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Controller('auth')
 export class AuthController {
+  private logger = new Logger();
   constructor(private authService: AuthService) {}
   @Post('/signup')
   signUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    this.logger.debug('signup');
     return this.authService.signUp(authCredentialsDto);
   }
 
@@ -15,6 +17,7 @@ export class AuthController {
   signIn(
     @Body() authCredentialsDto: AuthCredentialsDto,
   ): Promise<{ accessToken: string }> {
+    this.logger.debug('signin');
     return this.authService.signIn(authCredentialsDto);
   }
 
